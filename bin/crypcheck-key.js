85a34daa518707564def7f950bb48c581425fcb4
jainharsh21 <jainharsh270@gmail.com>
2020-07-31 11:47:56 +0530

    added sub commands of key

--- bin/crypcheck-key.js
+++ bin/crypcheck-key.js
@@ -1,8 +1,18 @@
-const program = require('commander')
+const program = require("commander");
 
 program
-    .command('set')
-    .description('Set API Key - Get at https://nomics.com')
-    .action(()=> console.log('hey from set'))
+  .command("set")
+  .description("Set API Key -- Get at https://nomics.com")
+  .action(() => console.log("hey from set"));
 
-program.parse(process.argv)
+program
+  .command("show")
+  .description("Show API Key")
+  .action(() => console.log("hey from show"));
+
+program
+  .command("remove")
+  .description("Remove API Key")
+  .action(() => console.log("hey from remove"));
+
+program.parse(process.argv);
