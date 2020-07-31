bb1b85d29970ab105c5d67bbb70349f1857d693e
jainharsh21 <jainharsh270@gmail.com>
2020-07-31 12:28:46 +0530

    added show jey and remove key

--- commands/key.js
+++ commands/key.js
@@ -22,10 +22,24 @@ const key = {
     }
   },
   show() {
-    console.log("hey from show");
+    try {
+      const keyManager = new KeyManager();
+      const key = keyManager.getKey();
+      console.log("Current API Key ", key.yellow);
+      return key;
+    } catch (err) {
+      console.log(err.message.red);
+    }
   },
   remove() {
-    console.log("hey from remove");
+    try {
+      const keyManager = new KeyManager();
+      keyManager.deleteKey();
+      console.log("Key Removed".blue);
+      return;
+    } catch (err) {
+      console.log(err.message.red);
+    }
   },
 };
 
