756ead8ad161295e01c9c9a2d5e2d91a105ae5ce
jainharsh21 <jainharsh270@gmail.com>
2020-07-31 11:52:00 +0530

    moving the function for subcommands of key to its seperate file

---
+++ commands/key.js
@@ -0,0 +1,13 @@
+const key = {
+  set() {
+    console.log("hey from set");
+  },
+  show() {
+    console.log("hey from show");
+  },
+  remove() {
+    console.log("hey from remove");
+  }
+};
+
+module.exports = key;
